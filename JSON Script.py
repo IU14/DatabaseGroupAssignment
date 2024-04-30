31bcf8f3f3dd71c57f34d435d92f070b7f043a42
CL614 <114404617+CL614@users.noreply.github.com>
2024-04-30 13:41:04 +0100

    Spreadsheet converted to JSON document

--- JSON Script.py
+++ JSON Script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Step 1: Read Excel Data
-excel_file = "C:/Users/langc/OneDrive - Canterbury CHrist Church University/Documents/Year3/Big Data/Movies Data.xlsx"
+excel_file = "Movies Data.xlsx"
 df = pd.read_excel(excel_file)
 
 # Step 2: Convert to JSON
